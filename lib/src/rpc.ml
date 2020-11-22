cf5f4879e8f349f1537a7b7a839ffd67830fe04c
mefyl <mefyl@gruntech.org>
2020-11-22 13:57:53 +0100

    Provide a decoder for `block_get` RPC response.

--- lib/src/rpc.ml
+++ lib/src/rpc.ml
@@ -1,4 +1,7 @@
+open Async
 open Core
+
+let ( let* ) = ( >>= )
 
 let block_get =
   let bin_query = [%bin_type_class: string]
@@ -31,3 +34,22 @@ let metadata_put =
   let bin_query = [%bin_type_class: string * string]
   and bin_response = [%bin_type_class: unit] in
   Async.Rpc.Rpc.create ~name:"put_metadata" ~version:0 ~bin_query ~bin_response
+
+let decode_block_get = function
+  | Result.Ok (Result.Ok (pipe, _)) -> (
+    Async.Pipe.read pipe >>= function
+    | `Eof
+    | `Ok (Core.Either.First _) ->
+      let* () =
+        Logs_async.err (fun m -> m "missing executable metadata in block_put")
+      in
+      Async.return None
+    | `Ok (Core.Either.Second executable) ->
+      let f = function
+        | Core.Either.First s -> s
+        | Core.Either.Second _ ->
+          (* Fail more gracefully *)
+          failwith "metadata in the middle of put_block data"
+      in
+      Async.return @@ Some (Async.Pipe.map ~f pipe, executable) )
+  | _ -> Async.return None
--- lib/src/rpc.mli
+++ lib/src/rpc.mli
@@ -12,3 +12,9 @@ val index_get : (string * string, string list option) Rpc.t
 val index_put : (string * string * string list, unit) Rpc.t
 
 val metadata_put : (string * string, unit) Rpc.t
+
+(** Transform the response of the block_get pipe RPC into an intelligible stream
+    of the file content and executable boolean *)
+val decode_block_get :
+     (((string, bool) Either.t Async.Pipe.Reader.t * _, _) Result.t, _) Result.t
+  -> (string Async.Pipe.Reader.t * bool) option Async_kernel.Deferred.t
